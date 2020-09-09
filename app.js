437de8d44dc3c2208a1f1d3895f67299c8df90ed
shine <shine130@vip.qq.com>
2020-09-09 09:19:37 +0800

    笔记编辑器的样式

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-const Note = {
+const Editor = {
+  data(){
+    return {
+      entity:this.entityObject
+    }
+  },
   template:`
-    <div class="item">
-      <div class="content">
-        <div class="header">{{entity.body}}</div>
+    <div class="ui form">
+      <div class="field">
+        <textarea v-model="entity.body" rows="5" placeholder="写点东西...">
+        </textarea>
       </div>
     </div>
   `,
+  props:['entityObject']
+}
+
+const Note = {
   props:[
     'entityObject'
   ],
   data(){
     return {
-      entity:this.entityObject
+      entity:this.entityObject,
+      open:false
     }
-  }
+  },
+  template:`
+    <div class="item">
+      <div class="content">
+        <div class="header" @click="open = !open">{{entity.body || '新建笔记'}}</div>
+        <div class="extra">
+          <editor v-if="open" :entityObject="entity"></editor>
+        </div>
+      </div>
+    </div>
+  `,
+  components:{Editor}
 }
 
 const Notes = {
@@ -28,12 +50,24 @@ const Notes = {
         <i class="paw icon"></i>
         Shine Notes App _Vue.js
       </h4>
-      <a class="ui right floated basic violet button" href="">添加笔记</a>
+      <a @click="create" class="ui right floated basic violet button" href="">添加笔记</a>
       <div class="ui divided items">
         <note v-for="entity in entities" :entityObject="entity" :key="entity.$loki"></note>
       </div>
     </div>
   `,
+  methods: {
+    create(){
+      loadCollection('notes')
+        .then((collection) => {
+          const entity = collection.insert({
+            body:''
+          })
+          db.saveDatabase();
+          this.entities.unshift(entity)
+        })
+    }
+  },
   created() {
     loadCollection('notes').then((collection) => {
       const _entities = collection.chain()
